29840d830210d562e6eff3c21c2fedd10f384b26
RadekRojik <89291142+RadekRojik@users.noreply.github.com>
2022-07-05 17:34:07 +0200

    Add files via upload

--- conk_reader.sh
+++ conk_reader.sh
@@ -1,10 +1,11 @@
 #! /usr/bin/env bash
 
 tmp=/dev/shm/conkys
-ls "$tmp" | while read -r neco
+while read -r neco
 do
   [ ! -e "$tmp/$neco/temp" ] && continue
   read -r temp < <(cat "$tmp/$neco/temp")
-  echo -n " $neco: $temp°C"
-done
-echo ""
+  # echo -n " $neco: $temp°C"
+  out="$out$neco: $temp°C "
+done <<<$(ls "$tmp")
+[ "$out" ] && echo $out
