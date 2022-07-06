11d3718b758688121ebbf9bbb8069d62950b9065
RadekRojik <89291142+RadekRojik@users.noreply.github.com>
2022-07-06 11:30:18 +0200

    Add files via upload

---
+++ hdd.sh
@@ -0,0 +1,25 @@
+#! /usr/bin/env bash
+
+out="|"
+
+cti () {
+while read -r neco
+do
+  read -r temp < <(cat $tmp/$neco/$tmp1 2> /dev/null || continue)
+  out="$out$neco|conkys $neco|${temp:0:2}|C||"
+done <<<$vystup
+}
+
+
+tmp=/sys/block
+tmp1="device/device/hwmon/*/temp1_input"
+vystup=`ls "$tmp" | grep '^nvme'`
+cti
+
+tmp=/dev/shm/conkys
+tmp1=temp
+vystup=`ls "$tmp"`
+cti
+
+[ "$out" ] && echo $out
+
